ae6aee35e9209fc6dabfbac6a74c67b28461b848
Breno Santos Figueiredo <brenos93@gmail.com>
2023-05-11 13:37:27 -0300

    Primeiro commit com fluxo de trabalho funcionando.

---
+++ ScriptFormat/format.py
@@ -0,0 +1,37 @@
+import openpyxl
+import re
+
+def format_xlsx():
+    # Carregue o arquivo Excel
+    workbook = openpyxl.load_workbook('./../Result.xlsx')
+
+    # Selecione a planilha desejada
+    worksheet = workbook['Sheet1']
+
+    # Defina uma lista com o nome das colunas que deseja formatar
+    colunas = ['B', 'D', 'F',  'H', 'J']
+
+    # Percorra todas as colunas desejadas e remova os números
+    for coluna in colunas:
+        # Selecione a coluna pelo cabeçalho
+        col = worksheet[coluna]
+        # Percorra as células na coluna e remova os números
+        for cell in col:
+            if cell.value is not None:
+                cell.value = re.sub(r'[^\w\s]|[\d]', '', str(cell.value))
+
+    colunas = ['C', 'E', 'G', 'I']
+
+    # Percorra todas as colunas desejadas e remova tudo que não é número
+    for coluna in colunas:
+        # Selecione a coluna pelo cabeçalho
+        col = worksheet[coluna]
+        # Percorra as células na coluna e remova tudo que não é número
+        for cell in col:
+            if cell.value is not None:
+                cell.value = re.sub(r'[^\d.\/\-]', '', str(cell.value))
+
+    # Salve o arquivo Excel formatado
+    workbook.save('./../FormattedResult.xlsx')
+
+format_xlsx()
